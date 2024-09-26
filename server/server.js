9e43faa82fcd21055b2d6f8f0b13f4047a91148a
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-26 08:47:34 +0500

    added not found page

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/dbConnection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use("/", express.static(path.join(__dirname, "public")));
 
 connectDB();
 
@@ -11,8 +13,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("*", (req, res) => {
-  res.status(404).send("Page not found");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.listen(PORT, () => {
