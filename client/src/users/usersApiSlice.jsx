b66291b0e9c54d5a1dc614698d8e677d533f19aa
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-27 09:15:45 +0500

    modify the code

--- client/src/users/usersApiSlice.jsx
+++ client/src/users/usersApiSlice.jsx
@@ -34,21 +34,17 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 
 export const { useGetUsersQuery } = usersApiSlice;
 
-// returns the query result object
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
 
-// creates memoized selector
 const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult.data // normalized state object with ids & entities
+  (usersResult) => usersResult.data
 );
 
-//getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
   selectIds: selectUserIds,
-  // Pass in a selector that returns the users slice of state
 } = usersAdapter.getSelectors(
   (state) => selectUsersData(state) ?? initialState
 );
