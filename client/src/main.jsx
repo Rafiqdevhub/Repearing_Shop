40a76d8633939fde989261ac7b08cbf6f627f795
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-27 08:34:49 +0500

    redux added to app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
