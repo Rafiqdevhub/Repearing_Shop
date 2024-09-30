5d337dc95c25d5e6cf5083dded9a38d2ea602e12
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-30 10:50:52 +0500

    added the protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,34 +12,49 @@ import NewUserForm from "./users/NewUserForm";
 import Prefetch from "./pages/Prefetch";
 import UsersList from "./users/UserList";
 import PersistLogin from "./pages/PersistLogin";
+import RequireAuth from "./app/auth/RequiredAuth";
+import { ROLES } from "./config/roles";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
+        {/* Protected Routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<Prefetch />}>
-            <Route path="dash" element={<DashLayout />}>
-              <Route index element={<Welcome />} />
+          <Route
+            element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+          >
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
+                <Route index element={<Welcome />} />
 
-              <Route path="users">
-                <Route index element={<UsersList />} />
-                <Route path=":id" element={<EditUser />} />
-                <Route path="new" element={<NewUserForm />} />
-              </Route>
+                <Route
+                  element={
+                    <RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />
+                  }
+                >
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
 
-              <Route path="notes">
-                <Route index element={<NotesList />} />
-                <Route path=":id" element={<EditNote />} />
-                <Route path="new" element={<NewNote />} />
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
               </Route>
+              {/* End Dash */}
             </Route>
-            {/* End Dash */}
           </Route>
         </Route>
+        {/* End Protected Routes */}
       </Route>
     </Routes>
   );
